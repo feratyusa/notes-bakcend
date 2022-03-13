d4ee1c1d04e24f29959ab0a2ac3034567959dd5f
feratyusa <prabuega55@gmail.com>
2022-03-13 14:05:40 +0700

    first commit

---
+++ src/notes.js
@@ -0,0 +1,15 @@
+/**
+ * Notes structure:
+ * {
+ *      id: string,
+ *      title: string,
+ *      createdAt: string,
+ *      updatedAt: string,
+ *      tags: array of string,
+ *      body: string,
+ * },
+ */
+
+const notes = [];
+
+module.exports = notes;
